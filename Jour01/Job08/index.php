53520f3b9d396b323a48e8353731a043a16e63c3
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-03 15:31:10 +0200

    test(index): add test for sommenombrespremiers function with specific numbers

---
+++ Jour01/Job08/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
+    <title>Jour 01 - Job 08</title>
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <h1>Job08</h1>
+        <h2>Consignes</h2>
+        <p>Créez une fonction “sommenombrespremiers” qui prend en paramètres deux variables.</p>
+        <p>Si ces deux variables sont des nombres premiers, alors la fonction retourne leur somme.</p>
+        <p>Sinon, la fonction retourne false.</p>
+     
+            
+        <h2>Résultat</h2>
+        <script>
+            // Définition des nombres
+            let nombre1 = 7;
+            let nombre2 = 17;
+
+
+            // Appel de la fonction "jourtravaille()"
+            console.log(sommenombrespremiers(nombre1, nombre2));
+        </script>
+       
+     
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
