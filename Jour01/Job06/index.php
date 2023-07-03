943622d22eebb64bf7628c7cce8212fe289067d6
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-03 12:15:36 +0200

    feat(index): add fizzbuzz function and call it in script

---
+++ Jour01/Job06/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
+    <title>Jour 01 - Job 06</title>
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <h1>Job06</h1>
+        <h2>Consignes</h2>
+        <p>Créez une <strong>fonction</strong> <em>fizzbuzz</em> qui ne prend pas de paramètre.</p>
+        <p>Dans cette fonction, affichez dans la console web les nombres de 1 à 151.</p>
+        <p>Remplacez certains nombres par un mot selon les conditions suivantes :</p>
+        <ul>
+            <li>Si le nombre est un multiple de 3, affichez “Fizz”.</li>
+            <li>Si le nombre est un multiple de 5, affichez “Buzz”.</li>
+            <li>Si le nombre est un multiple de 3 et de 5, affichez “FizzBuzz”</li>
+        </ul>
+            
+        <h2>Résultat</h2>
+        <script>
+            // Appel de la fonction
+            fizzbuzz();
+        </script>
+       
+     
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
