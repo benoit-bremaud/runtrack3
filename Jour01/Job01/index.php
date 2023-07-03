0b62b5269d47868909d1070ed2e7d321a23e2ff8
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-03 10:02:39 +0200

    feat(Index): add basic HTML structure and JavaScript popup

---
+++ Jour01/Job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/index.js"></script>
+    <title>Jour 01 - Job 01</title>
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+     
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
