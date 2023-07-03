4cafa6a44521b988b78aaff78ef9eef8bea19d7c
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-03 11:14:40 +0200

    feat(index):  Call JavaScript bissextile function and interpret result on console

---
+++ Jour01/Job04/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
+    <title>Jour 01 - Job 04</title>
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <h1>Job04</h1>
+        <h2>Consignes</h2>
+        <p>Maintenant que vous savez comment inclure du <strong>javascript</strong> et que vous maitrisez la <strong>console web</strong>, 
+            vous allez pouvoir explorer davantage la syntaxe, la grammaire et le lexique du langage <strong>javascript</strong>.</p>
+        <p>Pour l’ensemble des exercices suivants, vous devez rendre un fichier 
+            <strong>script.js</strong> contenant le rendu de l’exercice et un fichier <strong>index.php</strong> qui l’inclut.</p>
+        <p>Déclarez une <strong>fonction</strong> "bisextile" qui prend en paramètre une variable "année".</p>
+        <p>Si l’année est <u>bissextile</u>, la fonction retourne <strong>true</strong>, sinon elle retourne <strong>false</strong>.</p>
+
+        <h2>Formulaire</h2>
+        <script>
+            console.log(bissextile(2000));
+        </script>
+       
+     
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
