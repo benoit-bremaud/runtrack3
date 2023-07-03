7a365c332bc2f8827df1811c3b10fe91ccce5b34
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-03 11:40:12 +0200

    feat(index): add "afficherjourssemaines" function call

---
+++ Jour01/Job05/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
+    <title>Jour 01 - Job 05</title>
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <h1>Job05</h1>
+        <h2>Consignes</h2>
+        <p>Créez une <strong>fonction</strong> <em>"afficherjourssemaines"</em>.</p>
+        <p>Cette <strong>fonction</strong> ne prend pas de <strong>paramètre</strong>.</p>
+        <p>Créez un <strong>tableau de strings</strong> <em>"jourssemaines"</em> qui contient <u>l’ensemble des jours de la 
+            semaine</u>, du Lundi au Dimanche.</p>
+        <p>Ensuite à l’aide d’une <strong>boucle for (for!)</strong> Affichez un par un ces jours.</p>
+            
+      
+        <h2>Résultat</h2>
+        <script>
+            afficherjourssemaines();
+        </script>
+       
+     
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
