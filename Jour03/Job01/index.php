9db4e37ce0e929937271d45d7a1eae12a36e12cd
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-05 11:15:10 +0200

    feat(index): add buttons for displaying and hiding text

---
+++ Jour03/Job01/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Runtrack3</title>
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <main>
+        <h1>Jour 03 - Job 01</h1>
+        <h2>Consignes</h2>
+        <p>Créez une balise <strong>&lt;button&gt;</strong> et sélectionnez-la dans votre code <strong>JavaScript</strong>.</p>
+        <p>En cliquant sur le <strong>bouton</strong>, vous devrez faire apparaître le texte sur votre page <strong>HTML</strong> :</p>
+        <p><em>"Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie."</em></p>
+        <p>Ensuite créez un <strong>bouton</strong> qui servira à cacher tout l’élément <strong>html</strong>.</p>
+
+        <h2>Code</h2>
+        <h3>Version "Vanilla"</h3>
+
+        <button id="affiche">Afficher</button>
+
+        <button id="efface">Effacer</button>
+
+        <h2 class="text" id="article">"Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit, ensuite on prie"</h2>
+    
+    </main>
+    <footer>
+
+    </footer>
+</body>
+<!-- <script src=https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script> -->
+
+<script src="script.js"></script>
+
+</html>
