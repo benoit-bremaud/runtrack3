3159e4ed269b7a34310106d8ea6d30efc9b190ca
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-07-17 10:01:47 +0200

    feat(index): add button click event handler

---
+++ Jour02/Job03/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack3</title>
+</head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+    <main>
+        <h1>Jour 02 - Job 03</h1>
+        <h2>Consignes</h2>
+        <p></p>
+
+        <h2>Code</h2>
+
+        <button id="button">Cliquez ici</button>
+
+        <p id="compteur">0</p>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+<script src="script.js"></script>
+
+</html>
